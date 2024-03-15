7b9cdf81fad25400aa4a39b6d479baeccc10abf2
emreozogul <emreozogul13@gmail.com>
2024-03-15 03:08:36 +0300

    update : Ui updates
    cropper can be initiliazed with this commit , however need to fix upload

--- app.py
+++ app.py
@@ -1,27 +1,53 @@
 import eel
+import os
+import sqlite3
+
+
+# Initialize database connection
+conn = sqlite3.connect('con.db')
+c = conn.cursor()
+
+# Create table
+c.execute('''CREATE TABLE IF NOT EXISTS projects (id INTEGER PRIMARY KEY, name TEXT, author TEXT, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)''')
+conn.commit()
 
 @eel.expose
-def say_hello_py(x ):
-    print('Hello from %s ' % x )
+def add_project(name, author):
+    c.execute('INSERT INTO projects (name, author) VALUES (?, ?)', (name, author))
+    conn.commit()
+    return "Project added successfully!"
 
-user = []
+@eel.expose
+def get_projects():
+    c.execute('SELECT * FROM projects ORDER BY created_at DESC')
+    return c.fetchall()
 
-@eel.expose  # Eel function
-def set_title():  # Example to send data for javascript/html
-    return "Code example - Eel + Bootstrap 4 + MongoDb"
 
+def create_folder(folder_path):
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+        print(f"Folder '{folder_path}' created.")
+    else:
+        print(f"Folder '{folder_path}' already exists.")
 
-@eel.expose  # Eel function
-def get_users():
-    all_users = user
-    for x in all_users:
-        x.pop("_id")  # Remove objects id
-        all_users.append(x)
-    return all_users
+@eel.expose  # Expose this function to the JavaScript
+def create_new_folder(folder_name):
+    create_folder(folder_name)
+    
+@eel.expose
+def check_project_name(project_name):
+    c.execute('SELECT * FROM projects WHERE name = ?', (project_name,))
+    # check if the project name already exists
+    if c.fetchone():
+        return True
+    else:
+        return False
 
     
-
+    
+    
 if __name__ == '__main__':
     eel.init('web')  # Give folder containing web files
     eel.start('index.html', size=(1000, 800) )    # Start
+    
     
--- web/js/index.js
+++ web/js/index.js
@@ -1,33 +1,63 @@
+// General 
 function openNav() {
-    document.getElementById("sidebar").style.width = "150px";
-    for (var i = 0; i < document.getElementsByClassName("page").length; i++) {
-        document.getElementsByClassName("page")[i].style.marginLeft = "150px";
-    }
+  document.getElementById("sidebar").style.width = "150px";
+  for (var i = 0; i < document.getElementsByClassName("page").length; i++) {
+    document.getElementsByClassName("page")[i].style.marginLeft = "150px";
+  }
 }
 function closeNav() {
-    document.getElementById("sidebar").style.width = "0";
-    for (var i = 0; i < document.getElementsByClassName("page").length; i++) {
-        document.getElementsByClassName("page")[i].style.marginLeft = "0px";
-    }
+  document.getElementById("sidebar").style.width = "0";
+  for (var i = 0; i < document.getElementsByClassName("page").length; i++) {
+    document.getElementsByClassName("page")[i].style.marginLeft = "0px";
+  }
 }
 function showSection(sectionId) {
-    var sections = document.querySelectorAll('.page');
-    sections.forEach(function(section) {
-        section.style.display = 'none';
-    });
-    
-    document.getElementById(sectionId).style.display = 'flex';
-}
+  var sections = document.querySelectorAll('.page');
+  sections.forEach(function (section) {
+    section.style.display = 'none';
+  });
 
+  document.getElementById(sectionId).style.display = 'flex';
+}
+// Import
 function nextPageImport() {
-    document.getElementById('importPage1').style.display = 'none';
-    document.getElementById('importPage2').style.display = 'flex';
+  document.getElementById('importPage1').style.display = 'none';
+  document.getElementById('importPage2').style.display = 'flex';
 }
 function prevPageImport() {
   document.getElementById('importPage1').style.display = 'flex';
   document.getElementById('importPage2').style.display = 'none';
 }
-document.addEventListener('DOMContentLoaded', () => {
+function nextPageImport2() {
+  document.getElementById('importPage2').style.display = 'none';
+  document.getElementById('importPage3').style.display = 'flex';
+}
+
+function prevPageImport2() {
+  document.getElementById('importPage2').style.display = 'flex';
+  document.getElementById('importPage3').style.display = 'none';
+}
+
+
+document.addEventListener('DOMContentLoaded', async () => {
+  let projectData = await eel.get_projects();
+  console.log(projectData);
+  const projectList = document.getElementById('projects');
+
+
+
+  if (projectData.length > 0) {
+    projectData.forEach(project => {
+      const projectItem = document.createElement('li');
+      projectItem.textContent = project;
+      projectList.appendChild(projectItem);
+    });
+  } else {
+    const projectItem = document.createElement('li');
+    projectItem.textContent = "No projects found";
+    projectList.appendChild(projectItem);
+  }
+
 
   const openbtn = document.getElementById('openbtn');
   openbtn.addEventListener('click', openNav);
@@ -35,7 +65,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const closebtn = document.getElementById('closebtn');
   closebtn.addEventListener('click', closeNav);
 
-  document.querySelector('.navContainer').addEventListener('click', function(event) {
+  document.querySelector('.navContainer').addEventListener('click', function (event) {
     // Check if the clicked element is one of the divs you're interested in
     if (event.target.classList.contains('navItem')) {
       // Get the 'data-section' attribute of the clicked div
@@ -51,90 +81,133 @@ document.addEventListener('DOMContentLoaded', () => {
   const prevBtnImport = document.getElementById('prevBtnImport');
   prevBtnImport.addEventListener('click', prevPageImport);
 
+  const nextBtnImport2 = document.getElementById('nextBtnImport2');
+  nextBtnImport2.addEventListener('click', nextPageImport2);
 
+  const prevBtnImport2 = document.getElementById('prevBtnImport2');
+  prevBtnImport2.addEventListener('click', prevPageImport2);
 
-  const dropZone = document.getElementById('dropZone');
-  const preview = document.getElementById('preview');
-    
-  ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
-    dropZone.addEventListener(eventName, preventDefaults, false);
+  const sNewProjectBtn = document.getElementById('sNewProjectBtn');
+  sNewProjectBtn.addEventListener('click', showAddScreenProject);
+
+  const sProjectListBtn = document.getElementById('sProjectListBtn');
+  sProjectListBtn.addEventListener('click', showProjectList);
+
+  const addProjectForm = document.getElementById('addProjectForm');
+  addProjectForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    addProject();
   });
-  
-  function preventDefaults(e) {
-    e.preventDefault();
-    e.stopPropagation();
-  }
-  
-  ['dragenter', 'dragover'].forEach(eventName => {
-    dropZone.addEventListener(eventName, highlight, false);
-  });
-  
-  ['dragleave', 'drop'].forEach(eventName => {
-    dropZone.addEventListener(eventName, unhighlight, false);
-  });
-  
-  function highlight(e) {
-    dropZone.classList.add('highlight');
-  }
-  
-  function unhighlight(e) {
-    dropZone.classList.remove('highlight');
-  }
-  
-  dropZone.addEventListener('drop', handleDrop, false);
-  
-  function handleDrop(e) {
-    const dt = e.dataTransfer;
-    const files = dt.files;
-  
-    handleFiles(files);
-  }
-  
-  function handleFiles(files) {
-    ([...files]).forEach(previewFile);
-  }
+  var image = document.getElementById('image');
+  var canvas = document.getElementById('canvas');
   var cropper;
 
-  function initializeCropper(imageSrc) {
-    if (cropper) {
-        cropper.destroy();
-        cropper = null;
+
+  var buttons = document.querySelectorAll('.importButton');
+  buttons.forEach((button) => {
+    button.addEventListener('click', function (event) {
+      console.log(event.target.id);
+      document.getElementById('imageInput').click();
+      var files = event.target.files;
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        image.src = e.target.result;
+        image.style.display = 'block'; // Show the image
+
+        if (cropper) {
+          cropper.destroy(); // Destroy the old cropper instance
+        }
+        image.onload = function () {
+          cropper = new Cropper(image, {
+            aspectRatio: 1 / 1,
+            background: false,
+            highlight: false,
+            zoomable: false,
+            zoomOnTouch: false,
+            zoomOnWheel: false,
+            ready: function () {
+              var width = image.offsetWidth;
+              var height = image.offsetHeight;
+
+              canvas.width = width;
+              canvas.height = height;
+              canvas.getContext('2d').clearRect(0, 0, width, height);
+              canvas.getContext('2d').drawImage(
+                image,
+                0, 0, image.naturalWidth, image.naturalHeight,
+                0, 0, width, height
+              );
+            }
+          }
+          );
+        };
+      };
+      if (files && files.length > 0) {
+        reader.readAsDataURL(files[0]);
       }
-  
-      // Load the new image
-    var image = new Image();
-    image.src = imageSrc;
-    image.onload = function () {
-      canvas.width = image.width;
-      canvas.height = image.height;
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.drawImage(image, 0, 0, image.width, image.height);
-          
-      // Initialize Cropper.js on the image
-      cropper = new Cropper(canvas, {
-        aspectRatio: 1 / 1, // Optional: Set the aspect ratio
-        crop(event) {
-          console.log(event.detail.x);
-          console.log(event.detail.y);
-          console.log(event.detail.width);
-          console.log(event.detail.height);
-          console.log(event.detail.rotate);
-          console.log(event.detail.scaleX);
-          console.log(event.detail.scaleY);
-        },
-      });
-    };
-  }
-  function previewFile(file) {
-    let reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onloadend = function() {
-      let img = document.createElement('img');
-      img.src = reader.result;
-      img.addEventListener('click', () => {initializeCropper( reader.result);});
-      preview.appendChild(img);
-    };
-  }
+    });
+  });
+
+
+
+
 });
 
-  
+
+// project
+
+function createFolder() {
+  var folderName = document.getElementById('projectName').value;
+  eel.create_new_folder(folderName);  // Call the Python function
+}
+
+
+async function addProject() {
+  var name = document.getElementById('projectNameAdd').value;
+  var author = document.getElementById('authorAdd').value;
+
+  console.log(name);
+  console.log(author);
+
+  if ((name == '' || author == '') || (name == null && author == null)) {
+    alert('Please fill in all fields');
+    return;
+  }
+
+  let isOkey = await eel.check_project_name(name);
+  if (isOkey == false) {
+    alert('Project name already exists');
+    return;
+  }
+  let response = await eel.add_project(name, author)();
+  alert(response);
+
+  const projectlist = document.getElementById('project-list');
+  const projectItem = document.createElement('li');
+  projectItem.textContent = name;
+  projectlist.appendChild(projectItem);
+  showProjectList();
+}
+
+async function readProject() {
+  let response = await eel.read_project()();
+  const projectlist = document.getElementById('project-list');
+
+  response.forEach(project => {
+    const projectItem = document.createElement('li');
+    projectItem.textContent = project;
+    projectlist.appendChild(projectItem);
+  });
+
+}
+
+
+function showProjectList() {
+  document.getElementById('projectPage1').style.display = 'flex';
+  document.getElementById('projectPage2').style.display = 'none';
+}
+
+function showAddScreenProject() {
+  document.getElementById('projectPage1').style.display = 'none';
+  document.getElementById('projectPage2').style.display = 'flex';
+}
