8e63033b1d126bff874e920792a299a4b1aaaec0
Emre <74875617+emreevcin@users.noreply.github.com>
2024-04-27 18:28:02 +0300

    Model was updated
    We currently have approximately 79% accuracy. It will be aimed to increase this value by making updates and feature engineering in image processing.

--- prework/model.py
+++ prework/model.py
@@ -1,102 +1,219 @@
-import itertools
+# TODO Make the model available from within the application
+# TODO Try increasing accuracy
+# TODO Make the attributes more functional by using various plots, algorithms. Aiming to increase accuracy
+
 import os
-import numpy as np
 import pandas as pd
-from sklearn.calibration import LabelEncoder
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import normalize
-from sklearn.tree import DecisionTreeClassifier, plot_tree
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler, StandardScaler, normalize
+from sklearn.model_selection import train_test_split, GridSearchCV
 import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 
 # Takes the folder path as input and returns the path to the latest CSV file in that folder.
-def get_latest_csv_file(folder_path):
+def get_latest_csv_file():
     # Get a list of all files in the folder
-    files = os.listdir(folder_path)
-    
+    files = os.listdir(os.path.join(os.getcwd(), 'data'))
+    print(files)
+
     # Filter out only CSV files
     csv_files = [file for file in files if file.endswith('.csv')]
-    
+
     # Sort the CSV files based on their timestamps
-    sorted_files = sorted(csv_files, key=lambda x: os.path.getmtime(os.path.join(folder_path, x)), reverse=True)
-    
+    sorted_files = sorted(csv_files, key=lambda x: os.path.getmtime(os.path.join('data', x)), reverse=True)
+
     if sorted_files:
         # Return the path to the latest CSV file
-        return os.path.join(folder_path, sorted_files[0])
+        return os.path.join('data', sorted_files[0])
     else:
         return None
 
+
 # Load the dataset
-folder_path = os.path.join('prework', 'data')
-latest_csv_file = get_latest_csv_file(folder_path)
+latest_csv_file = get_latest_csv_file()
 if latest_csv_file:
     print("Latest CSV file: " + latest_csv_file)
-    df = pd.read_csv(latest_csv_file)
+    data = pd.read_csv(latest_csv_file)
 else:
     print("No CSV files found in the folder.")
-    
-# Assign features (X) and target labels (y)
-X = df.iloc[:, 1:-1]  # Select all columns except the last one as features
-y = df.iloc[:, -1]   # Select the last column as the target
 
-# Splitting the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=35)
+# Let's take a look at the first few rows of the dataset
+# print(data.head())
 
-# Creating the Decision Tree Classifier
-clf = DecisionTreeClassifier()
+# Check the size of the dataset
+# print("Dataset size:", data.shape)
 
-# Fitting the classifier to the training data
-clf.fit(X_train, y_train)
+# Get the statistical summary of the dataset
+# print("Statistical summary of the dataset:\n", data.describe())
 
-# Making predictions on the testing data
-y_pred = clf.predict(X_test)
+# Drop Image Name column
+data = data.drop(columns=['Image_Name'], axis=1)
 
-# Evaluating the model
-accuracy = accuracy_score(y_test, y_pred)
-print("Accuracy:", accuracy)
+# Select the categorical columns
+categorical_column = ['Target']
 
-# Feature importances
-feature_importances = pd.Series(clf.feature_importances_, index=X.columns)
+# One-hot encoding
+encoder = OneHotEncoder(sparse_output=False)
+one_hot_encoded = encoder.fit_transform(data[categorical_column])
 
-# Plotting feature importances as a bar chart
-plt.figure(figsize=(10, 6))
-feature_importances.plot(kind='bar')
-plt.title('Feature Importances')
-plt.xlabel('Features')
-plt.ylabel('Importance')
-plt.xticks(rotation=45)
-plt.show()
+# Create a dataframe from the one-hot encoded array
+one_hot_df = pd.DataFrame(one_hot_encoded, columns=encoder.get_feature_names_out(categorical_column))
 
-# Visualizing the decision tree
-plt.figure(figsize=(12, 8))
-class_names = np.unique(y).astype(str)
-plot_tree(clf, filled=True, feature_names=X.columns, class_names=class_names)
-plt.show()
+# Concatenate the one-hot encoded dataframe with the original dataframe
+df_encoded = pd.concat([data, one_hot_df], axis=1)
+
+# Drop the original categorical columns
+df_encoded = df_encoded.drop(categorical_column, axis=1)
+
+num_cols = [col for col in df_encoded.columns if
+            col not in ['Target_Extremely Death', 'Target_Healthy', 'Target_Slightly Death']]
+
+min_max_scaler = MinMaxScaler()
+df_encoded[num_cols] = min_max_scaler.fit_transform(df_encoded[num_cols])
+
+df_encoded[num_cols] = normalize(df_encoded[num_cols])
+
+standard_scaler = StandardScaler()
+df_encoded[num_cols] = standard_scaler.fit_transform(df_encoded[num_cols])
+
+# Let's create a loop to check for outliers
+for column in df_encoded.columns[:-3]:
+    # Calculate the first and third quartiles
+    Q1 = df_encoded[column].quantile(0.25)
+    Q3 = df_encoded[column].quantile(0.75)
+
+    # Calculate the lower and upper bounds for outliers
+    IQR = Q3 - Q1
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+
+    # Detect outliers
+    outliers = df_encoded[(df_encoded[column] < lower_bound) | (df_encoded[column] > upper_bound)]
+
+    # Remove rows with outliers completely
+    df_encoded = df_encoded.drop(outliers.index)
+
+    # Print out the outliers and their counts and percentages
+    #print("{}. Outliers:\n".format(column), outliers)
+    #print("{}. Number of Outliers: {}".format(column, outliers.shape[0]))
+    #print("{}. Percentage of Outliers: {:.2f}%\n".format(column, outliers.shape[0] / df_encoded.shape[0] * 100))
+
+#print("After removing rows with outliers, the size of the dataset is:", df_encoded.shape)
+
+# Separate independent variables (X) and target variable (y)
+X = df_encoded.drop(columns=['Target_Extremely Death', 'Target_Healthy', 'Target_Slightly Death'])
+y = df_encoded[['Target_Extremely Death', 'Target_Healthy', 'Target_Slightly Death']]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+
+# Define the parameter grid
+# param_grid_decision_tree = {
+#     'max_depth': [None, 5, 10, 20],
+#     'min_samples_split': [2, 5, 10],
+#     'min_samples_leaf': [1, 2, 4],
+#     'criterion': ['gini', 'entropy']
+# }
+#
+# # Define the parameter grid
+# param_grid_knn = {
+#     'n_neighbors': [3, 5, 7, 9, 11],
+#     'weights': ['uniform', 'distance'],
+#     'p': [1, 2]  # 1 for Manhattan distance, 2 for Euclidean distance
+# }
+#
+# # Define the parameter grid
+# param_grid_random_forest = {
+#     'n_estimators': [50, 100, 200],
+#     'max_depth': [None, 10, 20],
+#     'min_samples_split': [2, 5, 10],
+#     'min_samples_leaf': [1, 2, 4],
+#     'bootstrap': [True, False]
+# }
+
+# # Decision Trees
+# dt_model = DecisionTreeClassifier()
+# # Initialize GridSearchCV
+# grid_search = GridSearchCV(dt_model, param_grid_decision_tree, cv=5, scoring='accuracy', n_jobs=-1)
+# # Fit the grid search to the data
+# grid_search.fit(X_train, y_train)
+# # Get the best parameters
+# best_params = grid_search.best_params_
+# # Use the best model
+# best_dt_model = grid_search.best_estimator_
+# # Evaluate the best model
+# dt_pred = best_dt_model.predict(X_test)
+#
+# # K-Nearest Neighbors (KNN)
+# knn_model = KNeighborsClassifier()
+# # Initialize GridSearchCV
+# grid_search = GridSearchCV(knn_model, param_grid_knn, cv=5, scoring='accuracy', n_jobs=-1)
+# # Fit the grid search to the data
+# grid_search.fit(X_train, y_train)
+# # Get the best parameters
+# best_params = grid_search.best_params_
+# # Use the best model
+# best_knn_model = grid_search.best_estimator_
+# # Evaluate the best model
+# knn_pred = best_knn_model.predict(X_test)
+#
+# # Random Forests
+# rf_model = RandomForestClassifier()
+# # Initialize GridSearchCV
+# grid_search = GridSearchCV(rf_model, param_grid_random_forest, cv=5, scoring='accuracy', n_jobs=-1)
+# # Fit the grid search to the data
+# grid_search.fit(X_train, y_train)
+# # Get the best parameters
+# best_params = grid_search.best_params_
+# # Use the best model
+# best_rf_model = grid_search.best_estimator_
+# # Evaluate the best model
+# rf_pred = best_rf_model.predict(X_test)
+
+# TODO Try GridSearchCV for all of them
 
 
-# Confusion matrix
-conf_matrix = confusion_matrix(y_test, y_pred)
-print("Confusion Matrix:")
-print(conf_matrix)
+# Function to evaluate model performance
 
-# Plotting confusion matrix
-plt.figure(figsize=(8, 6))
-plt.imshow(conf_matrix, interpolation='nearest', cmap=plt.cm.Blues)
-plt.title('Confusion Matrix')
-plt.colorbar()
-tick_marks = np.arange(len(np.unique(y)))
-plt.xticks(tick_marks, np.unique(y))
-plt.yticks(tick_marks, np.unique(y))
+# Evaluate the performance of models
+models = {
+    'Decision Tree': DecisionTreeClassifier(),
+    'Random Forest': RandomForestClassifier(),
+    'KNN': KNeighborsClassifier(),
+    'Neural Network': MLPClassifier(max_iter=1000, random_state=42, solver='adam', learning_rate='adaptive', tol=1e-6, alpha=0.01, learning_rate_init=0.01, hidden_layer_sizes=(100,))
+}
 
-fmt = '.2f' if normalize else 'd'
-thresh = conf_matrix.max() / 2.
-for i, j in itertools.product(range(conf_matrix.shape[0]), range(conf_matrix.shape[1])):
-    plt.text(j, i, format(conf_matrix[i, j], fmt),
-             horizontalalignment="center",
-             color="white" if conf_matrix[i, j] > thresh else "black")
+# Train and evaluate each model
+for model_name, model in models.items():
+    print(f"Training {model_name}...")
+    model.fit(X_train, y_train)
+    print(f"Evaluating {model_name}...")
+    y_pred = model.predict(X_test)
 
-plt.ylabel('True label')
-plt.xlabel('Predicted label')
-plt.tight_layout()
-plt.show()
+    # Calculate evaluation metrics
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred, average='weighted')
+    recall = recall_score(y_test, y_pred, average='weighted')
+    f1 = f1_score(y_test, y_pred, average='weighted')
+
+    # Print results
+    print(f"Performance of {model_name}:")
+    print(f"Accuracy: {accuracy:.4f}")
+    print(f"Precision: {precision:.4f}")
+    print(f"Recall: {recall:.4f}")
+    print(f"F1 Score: {f1:.4f}")
+    print("\n")
+
+# Write to CSV file
+#df_encoded.to_csv('data.csv', index=False)
+
+correlation_matrix = df_encoded.corr()
+
+plt.figure(figsize=(10, 8))
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f", linewidths=0.5)
+plt.title('Correlation Matrix')
+#plt.show()
--- prework/processing.py
+++ prework/processing.py
@@ -34,6 +34,9 @@ def calculate_features(contour, scale_factor):
 # Process an image and return the features and contoured image
 def process_image(image_path, scale_factor):
     image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
+    if image is None:
+        print(f"Error: Unable to load image '{image_path}'")
+        return None, None
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
     equalized = clahe.apply(gray)
@@ -54,14 +57,16 @@ def process_image(image_path, scale_factor):
     
 # Process 4x images and return the features and contoured images
 def process_images_4x(image_dir, scale_factor):
-    image_paths = [os.path.join(image_dir, img) for img in os.listdir(image_dir) if img.endswith(('.tif', '.jpg', '.png'))]
+    image_paths = [os.path.join(image_dir, img) for img in os.listdir(image_dir) if img.endswith(('.tif', '.jpg', '.png')) and not img.startswith('.')]
     features_4x = []
     contoured_images_4x = []
 
     for path in image_paths:
         image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+        if image is None:
+            print(f"Error: Unable to load image '{path}'")
         edges = cv2.Canny(image, 30, 120)
-        kernel = np.ones((5, 5), np.uint8)
+        kernel = np.ones((11, 11), np.uint8)
         closing = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel, iterations=3)
         contours, _ = cv2.findContours(closing, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         if contours:
@@ -96,7 +101,7 @@ def count_particles(image, min_area=100, max_area=5000, circularity_thresh=0.5):
 
     return particle_count
 
-def count_particles_4x(image, min_area=50, max_area=2500, circularity_thresh=0.2):
+def count_particles_4x(image, min_area=100, max_area=2500, circularity_thresh=0.3):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     blurred = cv2.GaussianBlur(gray, (5, 5), 0)
     thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
@@ -139,7 +144,7 @@ def write_features_to_csv_mm(features, image_names, output_dir, target_values):
                     'Solidity': feature[5],
                     'Convexity': feature[6],
                     'Particle_Count': feature[7],
-                    'Target': target_values[i] if i < len(target_values) else None
+                    'Target': 'Extremely Death'
                 })
     return csv_file_path
 
@@ -167,21 +172,32 @@ def write_features_to_csv_4x(features, image_names, output_dir, target_values_4x
                 'Solidity': feature[5],
                 'Convexity': feature[6],
                 'Particle_Count': feature[7],
-                'Target': target_values
+                'Target': 'Extremely Death'
             })
     return csv_file_path
 
 # Set parameters for image processing.    
 scale_factor = 0.0786
-spheroid_image_dir = 'prework/assets/images'
-spheroid_image_dir_4x = 'prework/assets/images4x'
-output_dir = 'prework/data'
-image_names = os.listdir(spheroid_image_dir)
+
+# Get the directory of the current script
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Construct paths relative to the current directory
+spheroid_image_dir = os.path.join(current_dir, 'assets', 'images')
+spheroid_image_dir_4x = os.path.join(current_dir, 'assets', 'images4x')
+output_dir = os.path.join(current_dir, 'data')
+
+# Check if the directory exists
+if os.path.exists(spheroid_image_dir):
+    image_names = os.listdir(spheroid_image_dir)
+else:
+    print(f"The directory '{spheroid_image_dir}' does not exist.")
+
 spheroid_features_mm = []
 
-target_values_4x = ["1", "0", "1", "0", "1", "0", "1", "0", "1", "0"]
+target_values_4x = ""
 
-target_values = ["1"]
+target_values = ""
 
 # Process 4x images and write the features to a CSV file.
 features_4x, contoured_images_4x, image_names_4x = process_images_4x(spheroid_image_dir_4x, scale_factor)
@@ -205,6 +221,16 @@ if len(spheroid_features_mm) == len(image_names) and spheroid_features_mm:
 
 for i, (feature, contoured_image) in enumerate(spheroid_features_mm):
     img_name = image_names[i]
+    original_img_path = os.path.join(spheroid_image_dir, img_name)
+    original_img = cv2.imread(original_img_path, cv2.IMREAD_UNCHANGED)
+
+    if original_img is None:
+        print(f"Error: Unable to load original image '{original_img_path}'")
+        continue
+
+    if contoured_image is None:
+        print(f"Error: Contoured image is None for '{img_name}'")
+        continue
  
     plt.figure(figsize=(12, 6))
     original_img = cv2.imread(os.path.join(spheroid_image_dir, img_name), cv2.IMREAD_UNCHANGED)
@@ -224,6 +250,17 @@ for i, (feature, contoured_image) in enumerate(spheroid_features_mm):
 #Show 4x images with contours drawn on them
 for i, contoured_image in enumerate(contoured_images_4x):
     img_name = image_names_4x[i]
+    original_img_path = os.path.join(spheroid_image_dir_4x, img_name)
+    original_img = cv2.imread(original_img_path, cv2.IMREAD_UNCHANGED)
+
+    if original_img is None:
+        print(f"Error: Unable to load original image '{original_img_path}'")
+        continue
+
+    if contoured_image is None:
+        print(f"Error: Contoured image is None for '{img_name}'")
+        continue
+
     plt.figure(figsize=(12, 6))
     plt.subplot(1, 2, 1)
     plt.imshow(cv2.cvtColor(cv2.imread(os.path.join(spheroid_image_dir_4x, img_name), cv2.IMREAD_UNCHANGED), cv2.COLOR_BGR2RGB))
@@ -239,15 +276,15 @@ for i, contoured_image in enumerate(contoured_images_4x):
     
 
 # Run the model.py file
-python_path = sys.executable
-model_py_path = os.path.join('prework', 'model.py')
-
-if os.path.isfile(model_py_path):
-    try:
-        print("Running the model.py file...")
-        subprocess.run([python_path, model_py_path, csv_file_path_mm], check=True)
-        print("model.py file executed successfully.")
-    except subprocess.CalledProcessError as e:
-        print(f"An error occurred while running the model.py file: {e}")
-else:
-    print("Model.py file not found.")
+# python_path = sys.executable
+# model_py_path = os.path.join('prework', 'model.py')
+#
+# if os.path.isfile(model_py_path):
+#     try:
+#         print("Running the model.py file...")
+#         subprocess.run([python_path, model_py_path, csv_file_path_mm], check=True)
+#         print("model.py file executed successfully.")
+#     except subprocess.CalledProcessError as e:
+#         print(f"An error occurred while running the model.py file: {e}")
+# else:
+#     print("Model.py file not found.")
